0117577411880d90b8fc822774f9115eb7882101
jinkyminusbenquadinaros <s24000007739@leggott.ac.uk>
2025-04-28 10:26:38 +0100

    Add files via upload
    sligthly updated ui more finished need to use image in image folder to make add expenses tab

---
+++ Finance Project actual one ignore others/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Finance_Project_actual_one_ignore_others.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance_Project_actual_one_ignore_others.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public MainViewModel() 
+        { 
+
+        }
+    }
+}
