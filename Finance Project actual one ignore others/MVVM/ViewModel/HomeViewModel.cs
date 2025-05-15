fa9738b8316ae578968f97e5a347be3ab128bf41
jinkyminusbenquadinaros <s24000007739@leggott.ac.uk>
2025-05-15 15:04:47 +0100

    Add files via upload

---
+++ Finance Project actual one ignore others/MVVM/ViewModel/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance_Project_actual_one_ignore_others.MVVM.ViewModel
+{
+    internal class HomeViewModel
+    {
+    }
+}
--- Finance Project actual one ignore others/MVVM/ViewModel/MainViewModel.cs
+++ Finance Project actual one ignore others/MVVM/ViewModel/MainViewModel.cs
@@ -7,11 +7,51 @@ using System.Threading.Tasks;
 
 namespace Finance_Project_actual_one_ignore_others.MVVM.ViewModel
 {
-    internal class MainViewModel : ObservableObject
+    class MainViewModel : ObservableObject
     {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+        public RelayCommand RecurringExpenseCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
+        public RecurringExpenseModel RecurringVM { get; set; }
+
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+
+
         public MainViewModel() 
         { 
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+            RecurringVM = new RecurringExpenseModel();
+            CurrentView = HomeVM;
 
+            HomeViewCommand = new RelayCommand(o => 
+            { 
+            CurrentView = HomeVM;
+            });
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryVM; ;
+            });
+            RecurringExpenseCommand = new RelayCommand(o =>
+            {
+                CurrentView = RecurringVM;
+            });
+            
         }
     }
 }
